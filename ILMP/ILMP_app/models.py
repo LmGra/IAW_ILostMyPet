05451ce8a520fdd37f92015b4e2a50dd2b3acdcc
LmGra <61028324+LmGra@users.noreply.github.com>
2022-10-10 21:33:36 +0200

    Add files via upload

--- ILMP/ILMP_app/models.py
+++ ILMP/ILMP_app/models.py
@@ -1,3 +1,73 @@
+from multiprocessing.dummy import Manager
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Usuarios(models.Model):
+    nameUsr = models.CharField(max_length=200)
+    passUsr = models.CharField(max_length=200)
+    ubiUsr = models.CharField(max_length=200)
+    mailUsr = models.EmailField(max_length=254)
+    #phoneUsr = models.CharField(max_length=12)
+    #pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.nameUsr
+
+#GENDER=[("M"),("F")]
+#GENDER_CHOICES = [
+#    ('Male', 'Male'),
+#    ('Female', 'Female'),
+#]
+
+#class Gender(models.TextChoices):
+#    Man =   "MAN",("Man")
+#    Woman   =   "WOMAN",("WOMAN")
+
+#gender = models.CharField(
+#    max_length = 7,
+#    choices=Gender.choices,
+#    default=Gender.MAN,
+#)
+
+class Mascotas(models.Model):
+    namePet = models.CharField(max_length=200)
+    infoPet = models.CharField(max_length=200)
+    agePet = models.DateField(blank=True, null=True)
+    typePet = models.CharField(max_length=200)
+    imgPet = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    
+    #imgPet = models.BooleanField()
+    #models.PositiveBigIntegerField()
+    
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #genderPet = models.CharField(max_length=1, choices=GENDER)
+
+    def __str__(self):
+        return self.namePet
+
+class Perdidas(models.Model):
+    infoLost = models.CharField(max_length=200)
+    dateLost = models.DateTimeField(blank=True, null=True)
+    petLost = models.ForeignKey("Mascotas", on_delete=models.CASCADE, null=True)
+    usrLost = models.ForeignKey("Usuarios", on_delete=models.CASCADE, null=True)
+
+    #def __str__(self):
+        #return self.perdidas_text
+
+class Encuentros(models.Model):
+    typeFind = models.CharField(max_length=200)
+    #genderFind = models.CharField(max_length=1, choices=GENDER)
+    infoFind = models.CharField(max_length=200)
+
+    #def __str__(self):
+        #return self.encuentros_text
+
+class Area(models.Model):
+    country = models.CharField(max_length=200)
+    comunidadautonoma = models.CharField(max_length=200)
+    provincia = models.CharField(max_length=200)
+    calle = models.CharField(max_length=200)
+
+    #def __str__(self):
+        #return self.encuentros_text
