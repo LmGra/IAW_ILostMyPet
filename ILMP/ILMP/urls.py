476e59a7a008817821ef00d13e7c4d2c6fe8e4c8
LmGra <61028324+LmGra@users.noreply.github.com>
2022-10-14 17:26:45 +0200

    Add files via upload

--- ILMP/ILMP/urls.py
+++ ILMP/ILMP/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from ILMP_app import views
-from ILMP_app.views import MascotasListView, EncuentrosListView, PerdidosListView
+from ILMP_app.views import PerdidosDetailView, EncuentrosDetailView, MascotasDetailView, MascotasListView, EncuentrosListView, PerdidosListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mascotas/', MascotasListView.as_view()),
+    path('mascotas/<int:pk>/', MascotasDetailView.as_view(), name='mascotas-detail'),
     path('encuentros/', EncuentrosListView.as_view()),
+    path('encuentros/<int:pk>/', EncuentrosDetailView.as_view(), name='encuentros-detail'),
     path('perdidos/', PerdidosListView.as_view()),
+    path('perdidos/<int:pk>/', PerdidosDetailView.as_view(), name='perdidos-detail'),
 ]
--- ILMP/ILMP_app/views.py
+++ ILMP/ILMP_app/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
 from ILMP_app.models import Mascotas, Encuentros, Perdidos
 
 class MascotasListView(ListView):
     model = Mascotas
 
+class MascotasDetailView(DetailView):
+
+    model = Mascotas
+
+    #context_object_name = 'mascotas'
+    #queryset = Mascotas.objects.all()
+
+
 class EncuentrosListView(ListView):
     model = Encuentros
 
+class EncuentrosDetailView(DetailView):
+
+    model = Encuentros
+
 class PerdidosListView(ListView):
+    model = Perdidos
+
+class PerdidosDetailView(DetailView):
+
     model = Perdidos
 
 #def index(request):
